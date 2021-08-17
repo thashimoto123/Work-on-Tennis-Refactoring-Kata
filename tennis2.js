abd4289b4ab15ef723b9d12e8cf4843452c15bb1
thashimoto <thahsimoto@.me>
2021-08-17 20:48:05 +0900

    条件式に名前をつける。メイン関数を一番上に移動する。

--- tennis2.js
+++ tennis2.js
@@ -1,5 +1,14 @@
 'use strict';
 
+function getScore(P1point, P2point) {
+    if (isSameScore(P1point, P2point)) return getSameScoreName(P1point, P2point);
+    if (isNumberScore(P1point, P2point)) return scoreList[P1point] + "-" + scoreList[P2point];
+    if (isAdvantage(P1point, P2point))  return "Advantage " + getWinner(P1point, P2point);
+    return "Win for " + getWinner(P1point, P2point);
+}
+
+const scoreList = ["Love", "Fifteen", "Thirty", "Forty"];
+
 function getSameScoreName(P1point, P2point) {
     switch (P1point){
         case 0:
@@ -13,13 +22,17 @@ function getSameScoreName(P1point, P2point) {
     }
 }
 
-const scoreList = ["Love", "Fifteen", "Thirty", "Forty"];
 
-function getScore(P1point, P2point) {
-    if (P1point === P2point) return getSameScoreName(P1point, P2point);
-    if (P1point < 4 && P2point < 4) return scoreList[P1point] + "-" + scoreList[P2point];
-    if (Math.abs(P1point - P2point) === 1)  return "Advantage " + getWinner(P1point, P2point);
-    return "Win for " + getWinner(P1point, P2point);
+function isSameScore (P1point, P2point) {
+    return P1point === P2point;
+}
+
+function isNumberScore (P1point, P2point) {
+    return P1point < 4 && P2point < 4;
+}
+
+function isAdvantage (P1point, P2point) {
+    return Math.abs(P1point - P2point) === 1;
 }
 
 function getWinner(P1point, P2point) {
