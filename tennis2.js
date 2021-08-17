c6f67c98b8c714e74d0043a00653d66cc747295a
thashimoto <thahsimoto@.me>
2021-08-17 20:33:52 +0900

    プレイヤー名の取得を関数にまとめる

--- tennis2.js
+++ tennis2.js
@@ -18,8 +18,12 @@ const scoreList = ["Love", "Fifteen", "Thirty", "Forty"];
 function getScore(P1point, P2point) {
     if (P1point === P2point) return getSameScoreName(P1point, P2point);
     if (P1point < 4 && P2point < 4) return scoreList[P1point] + "-" + scoreList[P2point];
-    if (Math.abs(P1point - P2point) === 1)  return P1point > P2point ? "Advantage player1" : "Advantage player2";
-    return P1point > P2point ? "Win for player1" : "Win for player2";
+    if (Math.abs(P1point - P2point) === 1)  return "Advantage " + getWinner(P1point, P2point);
+    return "Win for " + getWinner(P1point, P2point);
+}
+
+function getWinner(P1point, P2point) {
+    return P1point > P2point ? "player1" : "player2"
 }
 
 module.exports = getScore;
