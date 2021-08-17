f93ffda5cfd2e15c3c5bf9dec7f8281d1cc97b99
thashimoto <thahsimoto@.me>
2021-08-17 20:26:29 +0900

    勝者を求める処理をひとつにまとめる

--- tennis2.js
+++ tennis2.js
@@ -17,21 +17,9 @@ const scoreList = ["Love", "Fifteen", "Thirty", "Forty"];
 
 function getScore(P1point, P2point) {
     if (P1point === P2point) return getSameScoreName(P1point, P2point);
-    let score;
     if (P1point < 4 && P2point < 4) return scoreList[P1point] + "-" + scoreList[P2point];
-
-    if (Math.abs(P1point - P2point) === 1) {
-        return P1point > P2point ? "Advantage player1" : "Advantage player2";
-    }
-
-    if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-        score = "Win for player1";
-    }
-    if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
-        score = "Win for player2";
-    }
-    return score;
+    if (Math.abs(P1point - P2point) === 1)  return P1point > P2point ? "Advantage player1" : "Advantage player2";
+    return P1point > P2point ? "Win for player1" : "Win for player2";
 }
-
 
 module.exports = getScore;
