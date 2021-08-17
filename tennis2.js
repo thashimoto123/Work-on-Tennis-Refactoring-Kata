310529d33ea91e106b938f7d82c8195866811366
thashimoto <thahsimoto@.me>
2021-08-17 20:03:07 +0900

    スコア名を配列でまとめる。

--- tennis2.js
+++ tennis2.js
@@ -1,7 +1,6 @@
 'use strict';
 
 function getSameScoreName(P1point, P2point) {
-    if (P1point !== P2point) return;
     switch (P1point){
         case 0:
             return "Love-All";
@@ -14,68 +13,16 @@ function getSameScoreName(P1point, P2point) {
     }
 }
 
-function getScore(P1point, P2point) {
-    let score = getSameScoreName(P1point, P2point);
+const scoreList = ["Love", "Fifteen", "Thirty", "Forty"];
 
+function getScore(P1point, P2point) {
+    if (P1point === P2point) return getSameScoreName(P1point, P2point);
+    let score;
     let P1res;
     let P2res;
-    if (P1point > 0 && P2point === 0) {
-        if (P1point === 1) {
-            P1res = "Fifteen";
-        }
-        if (P1point === 2) {
-            P1res = "Thirty";
-        }
-        if (P1point === 3) {
-            P1res = "Forty";
-        }
-
-        P2res = "Love";
-        score = P1res + "-" + P2res;
-    }
-    if (P2point > 0 && P1point === 0) {
-        if (P2point === 1) {
-            P2res = "Fifteen";
-        }
-        if (P2point === 2) {
-            P2res = "Thirty";
-        }
-        if (P2point === 3) {
-            P2res = "Forty";
-        }
-
-        P1res = "Love";
-        score = P1res + "-" + P2res;
-    }
-
-    if (P1point > P2point && P1point < 4) {
-        if (P1point === 2) {
-            P1res = "Thirty";
-        }
-        if (P1point === 3) {
-            P1res = "Forty";
-        }
-        if (P2point === 1) {
-            P2res = "Fifteen";
-        }
-        if (P2point === 2) {
-            P2res = "Thirty";
-        }
-        score = P1res + "-" + P2res;
-    }
-    if (P2point > P1point && P2point < 4) {
-        if (P2point === 2) {
-            P2res = "Thirty";
-        }
-        if (P2point === 3) {
-            P2res = "Forty";
-        }
-        if (P1point === 1) {
-            P1res = "Fifteen";
-        }
-        if (P1point === 2) {
-            P1res = "Thirty";
-        }
+    if (P1point < 4 && P2point < 4) {
+        P1res = scoreList[P1point];
+        P2res = scoreList[P2point];
         score = P1res + "-" + P2res;
     }
 
